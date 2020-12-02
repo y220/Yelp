723eba477104cb70f5d7d2b48179edb53da23327
JieShengm <jsheng27@wisc.edu>
2020-12-01 18:53:02 -0600

    Modify model

--- code/rpart_Model.R
+++ code/rpart_Model.R
@@ -1,13 +1,10 @@
+library("rpart")
+library("rpart.plot")
 business=read.csv("Attributes_pizza.csv")
-business=business[,c("business_id","stars","RestaurantsAttire",
-                     "RestaurantsTakeOut","BusinessAcceptsCreditCards","NoiseLevel",
-                     "GoodForKids","RestaurantsReservations","RestaurantsGoodForGroups",
-                     "RestaurantsPriceRange2","HasTV","Alcohol","BikeParking",
-                     "RestaurantsDelivery","OutdoorSeating","Caters","WiFi",
-                     "dessert","latenight","lunch","dinner","brunch","breakfast","garage",
-                     "street","validated","lot","valet","romantic","intimate","classy",
-                     "hipster","divey","touristy","trendy","upscale","casual")]
-for(i in c(4,5,7,8,9,11,13:16,18:37)){business[,i]=as.logical(business[,i])}
+business=business[,c("business_id","stars",
+                     "GoodForKids",
+                     "RestaurantsPriceRange2",
+                     "RestaurantsDelivery","Caters","WiFi")]
 
 words=read.csv("WordEmbedding_selected.csv",row.names = 1)
 freq=matrix(rep(NA,2039*106),nrow=2039)
@@ -17,28 +14,24 @@ for(i in 4:109){
 freq=data.frame(freq)
 freq$business_id=rownames(tapply(words[,i], as.factor(words$business_id), sum))
 colnames(freq)=c(colnames(words[,4:109]),"business_id")
-freq$attitude_positive_1=apply(freq[,c("wonderful","perfectly","excellent",
-                                       "fantastic","delicious","awesome",
-                                       "favorite","perfect","amazing","loved")],1,sum)
-freq$attitude_positive_2=apply(freq[,c("fun","happy")],1,sum)
-freq$attitude_negative_1=freq$terrible
-freq$attitude_negative_2=freq$bad
-#freq=freq[,c("business_id","attitude_positive_1","attitude_positive_2",
-             "attitude_negative_1","attitude_negative_2","super","hoagie","table","friendly","delivery",
-             "pie","drink","dessert","mozzarella")]
-freq[,c("wonderful","perfectly","excellent",
-         "fantastic","delicious","awesome",
-         "favorite","perfect","amazing","loved","fun","happy","terrible","bad","good","great")]=NULL
+freq=freq[,c("business_id", "hoagie", "table", "friendly", 
+             "delivery", "pie", "drink", "dessert", "mozzarella", 
+             "fresh",  "crispy", "greasy", "spicy","italian")]
+
 dat=merge(business,freq,by="business_id")
 
 rownames(dat)=dat$business_id
 dat$business_id=NULL
-tree=rpart(stars~.,dat)
+tree=rpart(stars~.,dat,cp=0.005)
 tree$variable.importance
+
+#### MSE ####
 sum((predict(tree)-dat$stars)^2)/2039
 hist(predict(tree)-dat$stars)
-predict(tree)
 qqnorm(predict(tree)-dat$stars)
-plot((predict(tree)-dat$stars))
+plot((predict(tree)-dat$stars),pch=20)
 
-rpart.plot(tree)
+#### Create tree plot ####
+jpeg("rpart.jpg")
+rpart.plot(tree,type=1,fallen.leaves = FALSE, tweak = 1.4, space = 1, gap=0.5)
+dev.off()
